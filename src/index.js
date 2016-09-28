37e3aa3f81ffdf697b38506511461a68823ead0f
jd-erreape <juandediosherrero@gmail.com>
2016-09-28 07:15:21 +0200

    tutorial - Filling data

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
 
 const API_KEY = 'AIzaSyDEX4y7GRRULIhytyroY05aoGdbbl4W2Ic';
 
-YTSearch({
-  key: API_KEY,
-  term: 'foyone',
-}, function (data) {
-  console.log(data);
-});
-
 // Create the component which produce some HTML
-const App = () => {
-  return(
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component{
+  constructor (props) {
+    super(props);
+
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'foyone'}, (videos) => {
+      this.setState({videos});
+    });
+  };
+
+  render () {
+    return(
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos}/>
+      </div>
+    );
+  };
 };
 
 //Take the HTML of the component and put it on the DOM
